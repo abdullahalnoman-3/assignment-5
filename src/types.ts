c987b3ef01b9b27b46d1fa30e1862ea4950cd437
AbdulaalNoman <abdullahalnomanfreelancer@gmail.com>
2026-09-12 03:46:18 +0600

    API/ডাটা

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Technology {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  difficulty: string;
+  rating: number;
+  icon: string;
+  badge: string;
+}
